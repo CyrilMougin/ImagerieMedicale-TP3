46dc84ee41a8849a7b16c81130a56ce864df7d2b
CyrilMougin <c.mougin@hotmail.fr>
2019-12-13 14:00:02 -0500

    Create IRM_fonctionnelle.py

---
+++ IRM_fonctionnelle.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 30 20:39:03 2019
+
+@author: cmoug
+"""
+
+import nibabel as nib
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+    
+np_img = nib.load("Data/fmri.nii")
+print(np_img)
+ 
+   
+
