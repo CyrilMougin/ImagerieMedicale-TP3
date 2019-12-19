f5d15fd40dc29fb3c90e1a03c3850db4fe5ba0f2
Julien <jbrochier@ensc.fr>
2019-12-19 10:21:05 -0500

    ajout fft maxime

--- main.py
+++ main.py
@@ -152,3 +152,25 @@ for i in range(len(img_data[0][0])):
     ax.set_yticks([])
     
     plt.show()
+    
+# Look at activation for 2 different voxels:
+signal1 = img_data[x_max, y_max, z_max, :]
+signal2 = img_data[0, 0, 0, :]
+time = np.arange(0, 340, 4)
+plt.plot(time, signal1, 'b')
+plt.plot(time, signal2, 'r')
+plt.legend(['Activation', 'No activation'], loc='best')
+plt.show()
+# Compute fft for a voxel:
+# input signal is 85 long, 2**7 = 128 seems to be enough
+freq_signal1 = np.squeeze(np.fft.fft(signal1, n=2**7))
+freq_signal2 = np.squeeze(np.fft.fft(signal2, n=2**7))
+timestep = 3.864
+freq = np.squeeze(np.fft.fftfreq(2**7, d=timestep))
+plt.plot(freq[:63], np.absolute(freq_signal1[:63]), 'b')
+plt.plot(freq[:63], np.absolute(freq_signal2[:63]), 'r')
+plt.xlim((0, 0.13))
+plt.ylim((-100, 500))
+plt.legend(['Activation', 'No activation'], loc='best')
+plt.xticks(np.arange(0, max(freq[:63]), 0.02))
+plt.show()
